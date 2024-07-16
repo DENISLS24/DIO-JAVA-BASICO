d88bc3d97787d57ee9ed596e30f7607454db07fc
denisls <denis.limma@yahoo.com.br>
2024-07-15 22:13:49 -0300

    new

---
+++ tipos-variaveis/src/testes.java
@@ -0,0 +1,11 @@
+public class testes {
+
+    public static void main(String[] args) {
+
+        boolean condicao = false;
+
+        while (condicao) {
+            System.out.println("executou ... ");
+        }
+    }
+}
